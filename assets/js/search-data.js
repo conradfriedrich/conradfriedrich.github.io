49b369622e26ba3c931df9402729f3ce822390e4
conradfriedrich <613087+conradfriedrich@users.noreply.github.com>
2025-04-02 17:30:34 +0000

    Deploying to gh-pages from @ conradfriedrich/conradfriedrich.github.io@6fcbae1ce42ef375e9c57f8cb6ae1f54365dd9bd 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-cv",
           title: "cv",
-          description: "My short CV. I&#39;m happy to send a longer CV on request.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
