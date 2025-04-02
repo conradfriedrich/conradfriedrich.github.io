e65ab177e3af9ed80abe24dcd1a635c097dff4ca
conradfriedrich <613087+conradfriedrich@users.noreply.github.com>
2025-04-02 14:22:14 +0000

    Deploying to gh-pages from @ conradfriedrich/conradfriedrich.github.io@14cc3b1c663686249995446e4d54883c7c362ced 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
@@ -28,16 +28,16 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+            },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-i-successfully-defended-my-phd-thesis-at-lmu-munich-with-summa-cum-laude-yay-smile",
+          title: 'I successfully defended my PhD thesis at LMU Munich, with “summa cum laude”....',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
